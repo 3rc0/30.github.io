7207f6a03db02816d3f9a06a2afd8af50faf40f9
3rc0 <43502863+3rc0@users.noreply.github.com>
2019-01-27 00:28:15 +0300

    Update t3st0r00.php

--- files/t3st0r00.php
+++ files/t3st0r00.php
@@ -1,3 +1,27 @@
 <?php
-echo"Hello \n World from t3st0r00.php page";
+
+class  Dbclass {
+private $servername;
+  private $username;
+  private $password;
+  private $dbname;
+  private $charset;
+  
+  public function connect(){
+  $this->servername = "localhost";
+    $this->username = "root";
+    $this->password = "123456";
+    $this->dbname = "testdb";
+    $this->charset = "utf8mb4";
+    
+    try {
+    $dns = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".#this->charset;
+      $pdo = new PDO ($dns, $this->username, $this->password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    }catch (\PDOException $e){
+    echo "Connection failed: ".$e->getMessage();
+    }
+  }
+}
 ?>
