dd78f1873b31b4951ed592988a68cbf4272c02c1
3rc0 <43502863+3rc0@users.noreply.github.com>
2019-04-08 23:36:13 +0300

    Update Filet3stf1l3.js

--- js/Filet3stf1l3.js
+++ js/Filet3stf1l3.js
@@ -1,4 +1,11 @@
 function firstAlert(){
 document.write(alert(" [@] CAUTION [@] \n Unstable website and does not suitable for normal user!!!!"));
 }
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
 
